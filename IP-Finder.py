f8bf1eda7ae78f27693cd45252583c15fa52d3e6
SecLab-CH <104432412+SecLab-CH@users.noreply.github.com>
2022-04-26 20:21:44 +0200

    Update IP-Finder.py

--- IP-Finder.py
+++ IP-Finder.py
@@ -68,9 +68,9 @@ def searching():
 
 def switch(argument):
 	print(stylize("--------------------------------------------------------------------", colored.fg("white")))
-	print("What Search Engines do you want use?(Please put "+" delimiter if you want to search on multiple Search Engnes) ")
+	print("[?] Which Search Engines do you want use?(Please put "+" delimiter if you want to search on multiple Search Engnes!) ")
 	print("s --> Shodan, c --> Censys, z --> ZoomEye, a --> all ")
-	choice = input('Enter our choice --> ')
+	choice = input('Enter your choice --> ')
 	searching()
 	output = []
 	choicelow=choice.lower()
